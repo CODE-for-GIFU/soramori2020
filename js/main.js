73bc42a0f5c78e48555a37091c390d303b67fbe9
石井哲治 <ishii.gifu403@gmail.com>
2021-01-10 08:55:27 +0900

    災害場所の写真とマッピングを追加

--- js/main.js
+++ js/main.js
@@ -60,11 +60,30 @@ function init() {
     };
     L.control.layers(baseMap).addTo(map_104);
 
+
+
     //災害地点JSON
+    let H30_URYO = "https://cfgifu.users.earthengine.app/view/gifu"
+    let H30_PATH = "./img/pos/h30/"
     disastars = [
-         {"lat":35.6580,"lon":137.0413,"name":"災害地点１","geelink":"http://yahoo.co.jp","img":"img/pos/pos01.png"}
-        ,{"lat":35.8080,"lon":137.3013,"name":"災害地点２","geelink":"http://yahoo.co.jp","img":"img/pos/pos02.png"}
-    ];
+       {  "lat" : "36.37307755",  "lon" : "137.1800354",  "name" : "（国）３６０号",  "higai" : "土砂流出",  "img" : "image10.jpeg",  "geelink" : "H30_URYO",  },
+       {  "lat" : "36.34132121",  "lon" : "137.1792141",  "name" : "コカ谷",  "higai" : "土砂流出",  "img" : "image15.png",  "geelink" : "H30_URYO",  },
+       {  "lat" : "36.09314497",  "lon" : "136.9354637",  "name" : "（国）１５６号",  "higai" : "土砂崩れ",  "img" : "image14.jpeg",  "geelink" : "H30_URYO",  },
+       {  "lat" : "36.25800645",  "lon" : "137.1454502",  "name" : "宮川",  "higai" : "護岸決壊",  "img" : "image6.jpeg",  "geelink" : "H30_URYO",  },
+       {  "lat" : "36.26934474",  "lon" : "137.3986111",  "name" : "（主）高山上宝線",  "higai" : "路側決壊",  "img" : "imagr7.jpeg",  "geelink" : "H30_URYO",  },
+       {  "lat" : "36.20031121",  "lon" : "137.2235634",  "name" : "瓜単川",  "higai" : "護岸決壊",  "img" : "image4.jpeg",  "geelink" : "H30_URYO",  },
+       {  "lat" : "36.13475673",  "lon" : "137.248567",  "name" : "苔川",  "higai" : "護岸決壊",  "img" : "image5.jpeg",  "geelink" : "H30_URYO",  },
+       {  "lat" : "36.12636137",  "lon" : "137.2470499",  "name" : "山王洞",  "higai" : "土砂崩れ",  "img" : "image11.jpeg",  "geelink" : "H30_URYO",  },
+       {  "lat" : "35.98418789",  "lon" : "136.9797218",  "name" : "（一）惣則高鷲線",  "higai" : "舗装破損",  "img" : "image9.jpeg",  "geelink" : "H30_URYO",  },
+       {  "lat" : "35.93646212",  "lon" : "136.8351072",  "name" : "（国）１５６号",  "higai" : "ブロック倒壊",  "img" : "image8.jpeg",  "geelink" : "H30_URYO",  },
+       {  "lat" : "35.75123964",  "lon" : "137.0893633",  "name" : "和良川",  "higai" : "護岸決壊",  "img" : "image3.jpeg",  "geelink" : "H30_URYO",  },
+       {  "lat" : "35.90756888",  "lon" : "137.2070009",  "name" : "上呂地区",  "higai" : "土砂崩れ",  "img" : "image13.jpeg",  "geelink" : "H30_URYO",  },
+       {  "lat" : "35.73036532",  "lon" : "137.1993812",  "name" : "中原小学校裏",  "higai" : "土砂崩れ",  "img" : "image12.jpeg",  "geelink" : "H30_URYO",  },
+       {  "lat" : "35.64127875",  "lon" : "137.1583055",  "name" : "（主）関金山線",  "higai" : "路側決壊",  "img" : "image2.jpeg",  "geelink" : "H30_URYO",  },
+   ];
+
+
+
 
     //ピン
     var pin = L.icon({
@@ -79,11 +98,11 @@ function init() {
     // layers["2018年7月"] = L.layerGroup();
     for (let i=0;disastars.length;i++) {
         let pos = disastars[i];
-        let marker = L.marker([pos.lat,pos.lon],{icon: pin}).addTo(map_104);
+        let marker = L.marker([parseFloat(pos.lat),parseFloat(pos.lon)],{icon: pin}).addTo(map_104);
         marker.bindPopup("<p>"
             + pos.name
-            + "<br/><img src='"+pos.img+"'><br/>"
-            + "<a href='"+pos.geelink+"' target='gee'>災害箇所（GEE解析）</a>"
+            + "<br/><img src='"+H30_PATH+pos.img+"'><br/>"
+            + "<a href='"+H30_URYO+"' target='gee'>災害箇所（GEE解析）</a>"
             +"</p>"
         );
     }
